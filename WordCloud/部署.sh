0004cc256de55e9f8996cc1d0c44354d81b3cb3d
杜少雄 <email@shaoxiongdu.cn>
2023-07-04 21:45:05 +0800

    词云图部署配置
    Signed-off-by: 杜少雄 <email@shaoxiongdu.cn>

--- WordCloud/部署.sh
+++ WordCloud/部署.sh
@@ -10,10 +10,10 @@ mv WordCloud-1.0-SNAPSHOT-jar-with-dependencies.jar word-cloud.jar
 
 # 上传文件  确保已经配置了SSH公钥身份验证
 
-scp './word-cloud.jar' root@114.115.209.223:'/home/jar/'
+scp './word-cloud.jar' root@192.168.31.40:'/home/jar/'
 
 # 执行
-ssh root@114.115.209.223 << EOF
+ssh root@192.168.31.40 << EOF
 
 # 后台启动上传的 JAR 文件
 cd /home/jar
